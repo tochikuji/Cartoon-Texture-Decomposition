97ebf9895cc9648b12fe844e5c5326c2ddd008f2
Aiga SUZUKI <ai-suzuki@aist.go.jp>
2018-11-29 15:16:06 +0900

    add setup.py

--- cartex/__init__.py
+++ cartex/__init__.py
@@ -1,3 +1,4 @@
 from cartex.tools import expect_valid_float_image
 from cartex.iterative_lpf import iterativeLPF
 from cartex.ltv import LTV, channelwiseLTV
+from cartex.decomposition import CartoonTextureDecomposition
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Cartoon+Texture Decomposition',
+    version='0.0.1',
+    packages=['cartex'],
+    url='',
+    license='Apache2',
+    author='Aiga SUZUKI',
+    author_email='tochikuji@gmail.com',
+    description='Python implementation of fast Cartoon + Texture decomposition'
+)
